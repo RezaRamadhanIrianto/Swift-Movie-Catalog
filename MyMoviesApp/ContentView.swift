93e96946fa7a24812b82eb9e9d66ecd48283c301
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2021-04-07 14:25:34 +0700

    Initial Commit

---
+++ MyMoviesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMoviesApp
+//
+//  Created by Reza Ramadhan Irianto on 07/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMoviesApp/MyMoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoviesAppApp.swift
+//  MyMoviesApp
+//
+//  Created by Reza Ramadhan Irianto on 07/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MyMoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
